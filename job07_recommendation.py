70e3c1e5a2a0836325783f7f05b3f210fb1df495
cschuadj721 <2djun@hanmail.net>
2025-02-07 14:49:10 +0900

    GUI app added

--- job07_recommendation.py
+++ job07_recommendation.py
@@ -2,19 +2,19 @@ import pandas as pd
 from sklearn.metrics.pairwise import linear_kernel
 from scipy.io import mmread
 import pickle
-from konlpy.tag import Okt
 from gensim.models import Word2Vec
-
-from job06_TFIDF import Tfidf_matrix
-
 
 def getRecommendation(cosine_sim):
     simScore = list(enumerate(cosine_sim[-1]))
     simScore = sorted(simScore, key=lambda x:x[1], reverse = True)
+
+    #자기 자신 포함 11개
     simScore = simScore[:11]
     movieIdx = [i[0] for i in simScore]
 
     recmovieList = df_reviews.iloc[movieIdx, 0]
+
+    #자기 자신 빼고 10개 리턴
     return recmovieList[1:11]
 
 df_reviews = pd.read_csv('./crawling_data/cleaned_reviews.csv')
@@ -23,17 +23,45 @@ Tfidf_matrix = mmread('./models/moviereview.mtx').tocsr()
 with open('./models/tfidf.pickle', 'rb') as f:
     Tfidf = pickle.load(f)
 
-#영화 10번으로 테스트
-ref_idx = 349
-print(df_reviews.iloc[ref_idx, 0])
-cosine_sim = linear_kernel(Tfidf_matrix[ref_idx], Tfidf_matrix)
+# #영화 10번으로 테스트
+# ref_idx = 349
+# print(df_reviews.iloc[ref_idx, 0])
+#
+# #전체 영화 리스트에 대해 cosine sim 계산
+# cosine_sim = linear_kernel(Tfidf_matrix[ref_idx], Tfidf_matrix)
+#
+# print(cosine_sim[0:5])
+# print(len(cosine_sim))
+#
+# recommandations = getRecommendation(cosine_sim)
+# print(recommandations)
+#
+# # df_reviews.info()
 
-print(cosine_sim[0:5])
-print(len(cosine_sim))
 
-recommandations = getRecommendation(cosine_sim)
-print(recommandations)
+embedding_model =Word2Vec.load('./models/word2vec_movie_review.model')
+keyword = '공포'
+if keyword in list(embedding_model.wv.index_to_key):
+    sim_word = embedding_model.wv.most_similar(keyword, topn=10)
+    words = [keyword]
+    for word, _ in sim_word:
+        words.append(word)
+    print(words)
+else:
+    print('no word')
+    exit()
 
-# df_reviews.info()
+# 유사도 높은 단어 순으로 반복하는 임의의 문장 만들기
+sentence = []
+count = 10
+for word in words:
+    sentence = sentence + [word] * count
+    count -= 1
+sentence = ' '.join(sentence)
+print(sentence)
 
+sentence_vec = Tfidf.transform([sentence])
+cosine_sim = linear_kernel(sentence_vec, Tfidf_matrix)
+recommendation = getRecommendation(cosine_sim)
 
+print(recommendation)
